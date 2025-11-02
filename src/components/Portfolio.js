ca67c62a64f3cc4b61fde7eea3fc407a96d770a7
Ari Fikri <arirushan79@gmail.com>
2025-11-02 12:31:54 +0700

    make the portfolio section into 4 columns

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,12 +7,34 @@ const Portfolio = () => {
       <p style={{ color: 'var(--muted)', marginTop: 6 }}>A selection of recent weddings and couples sessions.</p>
 
       <div className="portfolio-grid">
-        <div className="port-item"><img src="https://plus.unsplash.com/premium_photo-1663076211121-36754a46de8d" alt="p1"/></div>
-        <div className="port-item"><img src="https://plus.unsplash.com/premium_photo-1681841695231-d674aa32f65b" alt="p2"/></div>
-        <div className="port-item"><img src="https://images.unsplash.com/photo-1591604466107-ec97de577aff" alt="p3"/></div>
-        <div className="port-item"><img src="https://images.unsplash.com/photo-1481653125770-b78c206c59d4" alt="p4"/></div>
-        <div className="port-item"><img src="https://plus.unsplash.com/premium_photo-1663076211121-36754a46de8d" alt="p5"/></div>
-        <div className="port-item"><img src="https://images.unsplash.com/photo-1591604466107-ec97de577aff" alt="p6"/></div>
+        {/* Column 1 */}
+        <div className="port-item">
+          <h3 className="port-title">Event Title 1</h3>
+          <img src="https://plus.unsplash.com/premium_photo-1663076211121-36754a46de8d" alt="p1"/>
+          <p className="port-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <a href="#" className="port-link">more &gt;&gt;</a>
+        </div>
+        {/* Column 2 */}
+        <div className="port-item">
+          <img src="https://plus.unsplash.com/premium_photo-1681841695231-d674aa32f65b" alt="p2"/>
+          <p className="port-description">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <a href="#" className="port-link">more &gt;&gt;</a>
+          <h3 className="port-title">Event Title 2</h3>
+        </div>
+        {/* Column 3 */}
+        <div className="port-item">
+          <p className="port-description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+          <a href="#" className="port-link">more &gt;&gt;</a>
+          <img src="https://images.unsplash.com/photo-1591604466107-ec97de577aff" alt="p3"/>
+          <h3 className="port-title">Event Title 3</h3>
+        </div>
+        {/* Column 4 */}
+        <div className="port-item">
+          <h3 className="port-title">Event Title 4</h3>
+          <p className="port-description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <a href="#" className="port-link">more &gt;&gt;</a>
+          <img src="https://images.unsplash.com/photo-1481653125770-b78c206c59d4" alt="p4"/>
+        </div>
       </div>
     </section>
   );
